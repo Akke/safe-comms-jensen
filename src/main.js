427903191399d172b6371949c8172de8687585a6
Akke <sirpurpleness@gmail.com>
2024-12-10 12:28:30 +0100

    feat: added basic route structure

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ require("dotenv").config()
 
 const { connectDb } = require("./config/db")
 
-connectDb()
-
 const app = express()
 
 const PORT = process.env.APP_PORT
+
+connectDb()
+
+app.use(express.json())
+
+const authRoutes = require("./routes/authRoutes")
+
+app.use("/auth", authRoutes)
 
 app.listen(PORT, () => {
 	console.log(`App running and listening on port ${PORT}`)
