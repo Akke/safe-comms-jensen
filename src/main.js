019b1ed8394a1fcc05d434a2dd8e2931b7d70aad
Axel Lindgren Wenell <atomwar.box@gmail.com>
2024-12-11 12:18:42 +0100

    feat: added routes into main.js

--- src/main.js
+++ src/main.js
@@ -11,9 +11,11 @@ connectDb()
 
 app.use(express.json())
 
-const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
+const inviteRoutes = require("./routes/inviteRoutes")
 
-app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/invite", inviteRoutes)
 
 app.listen(PORT, () => {
 	console.log(`App running and listening on port ${PORT}`)
