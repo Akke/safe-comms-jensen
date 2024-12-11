ab934d6c7e1f438082556ad7146f91e9d5948452
Axel Lindgren Wenell <atomwar.box@gmail.com>
2024-12-11 13:02:49 +0100

    feat: upgdated inviteUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,22 @@
 const User = require("../models/userModel")
+const Invite = require("../models/inviteModel")
 
 const inviteUser = async (req, res) => {
-	const { userId } = req.body
+	const { userId } = req.params
+	const { conversationId } = req.body
 
+	const sender = req.user.id
+	
+	try {
+		const newInvite = new Invite({ senderId: sender, recipientId: userId, conversationId: conversationId })
 
+		const saveInvite = await newInvite.save()
+
+		res.status(201).json(saveInvite)
+	} catch (error) {
+		console.log(error)
+		res.status(500).send("Server Error")
+	}	
 }
 
 module.exports = {
