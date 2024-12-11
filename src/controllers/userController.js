b83ac3325defe6c7c741f52e21b0695cae8af6d2
Klara Frommelin <freakyfrommelin@outlook.com>
2024-12-11 13:52:00 +0100

    Implementing getUser function
    Getting single user based on incoming userId

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,18 @@
 const User = require("../models/userModel")
 
-module.exports = {}
+const getUser = async ( req, res ) => {
+    const { userId } = req.body
+
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'})
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+}
+
+module.exports = { getUser }
