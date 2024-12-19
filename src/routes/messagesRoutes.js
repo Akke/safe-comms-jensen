14bd0620eef0618c23068e74796cfcf75c9d1749
ilyafredriksson <fredrikssonilya@gmail.com>
2024-12-19 11:18:09 +0100

    added models schema

---
+++ src/routes/messagesRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const {getMessages} = require('../controllers/messageController')
+
+router.get ('/',getMessages)
+module.exports =router
---
+++ src/schemas/messageSchema.js
@@ -0,0 +1,9 @@
+const mongoose =require('mongoose')
+
+const messageSchema = {
+	senderId:{type: mongoose.Schema.Types.ObjectId},
+	recipientId:{type:mongoose.Schema.Types.ObjectId}
+	message:{ type:String}
+}
+
+module.exports=messageSchema
