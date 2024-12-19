95fbc8dbcff37aef3ef7116c61bb2359c4a8ff35
Malin Bagewitz Fungdal <malinfungdal@hotmail.com>
2024-12-19 14:52:42 +0100

    fixat Usercontroller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getUser, updateUser, createUser } = require('../controllers/userController')
+const { getUser, updateUser, createUser, getAllUsers } = require('../controllers/userController')
 
 router.get('/:userId', getUser)
 router.put('/:userId', updateUser)
 router.post("/", createUser)
+router.get('/', getAllUsers)
 
 module.exports = router
