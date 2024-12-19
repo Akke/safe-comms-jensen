ec62074b5fa45d000244337c2bc2637736dd64aa
Akke <sirpurpleness@gmail.com>
2024-12-19 14:14:13 +0100

    feat: added userRoute for userCreate

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getUser, updateUser } = require('../controllers/userController')
+const { getUser, updateUser, createUser } = require('../controllers/userController')
 
 router.get('/:userId', getUser)
 router.put('/:userId', updateUser)
+router.post("/", createUser)
 
 module.exports = router
