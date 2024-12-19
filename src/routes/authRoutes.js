69909f9cfd1b92a4426d9512392c524b4c1b8173
hampusvh <hampus.vh@gmail.com>
2024-12-19 14:16:12 +0100

    updated authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,13 @@
 const express = require("express")
-const router = express.Router()
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({ message: 'Access denied!' });
+    }
+}
 
 module.exports = router
