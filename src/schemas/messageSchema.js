634617ae4a18b6ef3f40cc2494ece4de47eaaa28
Akke <sirpurpleness@gmail.com>
2024-12-19 11:26:35 +0100

    fix: fixed broken schema format

--- src/schemas/messageSchema.js
+++ src/schemas/messageSchema.js
@@ -1,9 +1,9 @@
 const mongoose =require('mongoose')
 
 const messageSchema = {
-	senderId:{type: mongoose.Schema.Types.ObjectId},
-	recipientId:{type:mongoose.Schema.Types.ObjectId}
-	message:{ type:String}
+	senderId: { type: mongoose.Schema.Types.ObjectId },
+	recipientId: { type: mongoose.Schema.Types.ObjectId },
+	message: { type: String }
 }
 
-module.exports=messageSchema
+module.exports = messageSchema
