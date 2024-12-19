a48bf797541a2a6b26b7cf2ddb33570e1a9e01ea
Akke <sirpurpleness@gmail.com>
2024-12-19 10:56:52 +0100

    added postMessage

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,11 +1,25 @@
 const  Message = require('../models/messageModel')
 
-const getMessages =async (req,res)=>{
-try { 
-	const messages =await Message.find();
-	res.json(messages);
+const getMessages = async (req, res) => {
+	try { 
+		const messages =await Message.find();
+		res.json(messages);
+	}  catch (err) {
+	res.status(500).json({error :'fel meddelande'});
+	}
+}
 
+const postMessage = async (req, res) => {
+	const { recipientId, senderId, message } = req.body
 
-}  catch (err) {
-res.status(500).json({error :'fel meddelande'});
+	try {
+		const newMessage = new Message({ recipientId, senderId, message })
+
+		const saveMessage = newMessage.save()
+
+		res.status(201).json(saveMessage)
+	} catch (error) {
+		console.log(error)
+		res.status(500).send("Server Error")
+	} 
 }
