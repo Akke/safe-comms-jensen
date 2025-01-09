af34334a3ad201de10bb90d52b22d989054d0423
Fehed Shushan <fehed165@gmail.com>
2025-01-09 11:22:49 +0100

    lagt en delete router

---
+++ src/routes/deleteRoutes.js
@@ -0,0 +1,10 @@
+const express= require("express");
+const { deleteuser } = require("../controllers/deleteController");
+const router= express.Router();
+const {
+    deleteuser,
+} = require("../controller/controller");
+
+router.delete("/:text_id" , deleteuser) ;
+
+module.exports =  router ;
