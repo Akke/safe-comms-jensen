fb320520c38d1275b85f9f3fa84169e887b2490a
hampusvh <hampus.vh@gmail.com>
2025-01-09 11:35:25 +0100

    commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,7 @@
 const express = require("express")
-const router = express.Router()
-const { createUser } = require("../controllers/userController.js")
-const jwt = require('jsonwebtoken');
+const router = express.Router();
+const { login } = require('../controllers/authController')
 
-const verifyToken = (req, res) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(403).json({ message: 'Access denied!' });
-    }
-}
-
-router.use("/register", createUser)
+router.post('/login', login)
 
 module.exports = router
