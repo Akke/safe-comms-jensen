b0b26b3b796ab90966191c5bcc204eec7742f086
Akke <sirpurpleness@gmail.com>
2025-01-14 09:29:04 +0100

    feat: added createUser route to authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const { createUser } = require("../controllers/userController")
 
 router.post("/login", login);
+router.post("/register", createUser);
 
 module.exports = router;
