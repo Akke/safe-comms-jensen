def61546254b838cfdec560257d76d1a9a653103
Akke <sirpurpleness@gmail.com>
2025-01-14 09:27:31 +0100

    fix: fixed conversation code being in authController?

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,53 +5,29 @@ const User = require("../models/userModel")
 const login = async (req, res) => {
     const { username, password } = req.body;
 
-
     try {
-    const findUser = await User.findOne ({ username })
-    if (!findUser) {
-        return res.status(404).json({ message: 'Användare finns inte' })
+        const findUser = await User.findOne ({ username })
+        if (!findUser) {
+            return res.status(404).json({ message: 'Användare finns inte' })
+        }
+
+        const checkPassword = await bcrypt.compare(password, findUser.password) 
+        if (!findUser.password) {
+            return res.status(401).json({ message: 'Ogiltigt lösenord' })
+        }
+
+        const token = jwt.sign(
+            { id: findUser._id, username: user.username },
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' }
+        )
+
+        res.status(200).json({token})
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Server error' })
     }
-
-    const checkPassword = await bcrypt.compare(password, findUser.password) 
-    if (!findUser.password) {
-        return res.status(401).json({ message: 'Ogiltigt lösenord' })
-    }
-
-    const token = jwt.sign(
-        { id: findUser._id, username: user.username },
-        process.env.JWT_SECRET, 
-        { expiresIn: '1h' }
-    )
-
-    res.status(200).json({token})
-
-} catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Server error' })
 }
-}
 
-const conversation = require('../models/conversationModel');
-
-
-const getAllConversations = async (req, res) => {
-    try {
-        const conversations = await conversation.find();
-        res.json(conversations);
-    } catch (err) {
-        res.status(500).json({ error: 'Kunde inte hämta konversationer.'});
-    }
-};
-
-const createConversation = async (req, res) => {
-    try {
-        const { title, participants } = req.body;
-        const newConversation = new Conversation ({ title, participants });
-        await newConversation.save();
-        res.status(201).json(newConversation);
-    } catch (err) {
-        res.status(500).json({ error: 'Kunde inte skapa konversation! '});
-    }
-};
-
-module.exports = { getAllConversations, createConversation, login }; 
+module.exports = { login }; 
