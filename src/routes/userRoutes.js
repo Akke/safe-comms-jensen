7313f4f4e44ce87b9534a78ff076bf91667c8ddf
Akke <sirpurpleness@gmail.com>
2025-01-14 09:29:33 +0100

    feat: removed createUser from userRoutes.js (moved)

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getUser, updateUser, createUser, getAllUsers } = require('../controllers/userController')
+const { getUser, updateUser, getAllUsers } = require('../controllers/userController')
 const { requireAuth } = require('../middleware/authMiddleware')
 
 router.get('/:userId', getUser)
 router.put('/:userId', requireAuth, updateUser)
-router.post("/", createUser)
 router.get('/', getAllUsers)
 
 module.exports = router
