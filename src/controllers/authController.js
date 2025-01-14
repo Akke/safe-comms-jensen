4fd850c6cc08c0f3bb10f62d8747071603e1ff2e
Akke <sirpurpleness@gmail.com>
2025-01-14 09:31:11 +0100

    fix: fixed jwt error

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ const login = async (req, res) => {
         }
 
         const token = jwt.sign(
-            { id: findUser._id, username: user.username },
+            { id: findUser._id, username: findUser.username },
             process.env.JWT_SECRET, 
             { expiresIn: '1h' }
         )
