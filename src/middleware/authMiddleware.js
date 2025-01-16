9dd21931010a8c4ec30fb1152d583e4d208cfa9a
hampusvh <hampus.vh@gmail.com>
2025-01-16 11:26:01 +0100

    added cookies handling to authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,11 +3,17 @@ const jwt = require("jsonwebtoken")
 const requireAuth = (req, res, next) => {
 	const authHeader = req.headers.authorization
 
-	if(!authHeader || !authHeader.startsWith("Bearer ")) {
+	let token;
+
+	if (authHeader && authHeader.startsWith("Bearer ")) {
+		token = authHeader.split(" ")[1];
+	} 
+	else if (req.cookies && req.cookies.token) {
+		token = req.cookies.token;
+	}
+	if (!token) {
 		return res.status(401).json({ message: "Authorization token required." })
 	}
-
-	const token = authHeader.split(" ")[1]
 
 	try {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET)
@@ -18,6 +24,7 @@ const requireAuth = (req, res, next) => {
 		console.error(error)
 		res.status(401).json({ message: "Invalid or expired token." })
 	}
-}
+};
 
 module.exports = { requireAuth }
+
